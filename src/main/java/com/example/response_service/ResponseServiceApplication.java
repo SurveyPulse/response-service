11d556862d9f50f64b30bb8bb3fad71e12ddf7b1
201912025 <michi2012@naver.com>
2025-04-03 12:28:54 +0900

    fix: BaseEntity 적용 오류 해결

--- src/main/java/com/example/response_service/ResponseServiceApplication.java
+++ src/main/java/com/example/response_service/ResponseServiceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableDiscoveryClient
 @EnableFeignClients
 @ComponentScan(basePackages = {"com.example.response_service", "com.example.global"})
+@EnableJpaAuditing
 public class ResponseServiceApplication {
 
 	public static void main(String[] args) {
